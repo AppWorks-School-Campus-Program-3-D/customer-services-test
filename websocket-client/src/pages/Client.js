315cbf3dafaa897c5b7c6df3c2b3c712feb121d7
李芸芳 <b08502041@csie.ntu.edu.tw>
2023-04-17 10:57:06 +0800

    feat: 🎸 add Client css and make it more like messager

--- websocket-client/src/pages/Client.js
+++ websocket-client/src/pages/Client.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import '../styles/Client.css'
 
 const Client = () => {
   const [userName, setUserName] = useState('')
@@ -7,6 +8,7 @@ const Client = () => {
   const [socket, setSocket] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const [messages, setMessages] = useState([])
+  const [showMessages, setShowMessages] = useState(false)
 
   const handleLogin = e => {
     e.preventDefault()
@@ -26,17 +28,19 @@ const Client = () => {
   }
 
   const handleSendMessage = e => {
-    e.preventDefault()
+    e.preventDefault();
     if (socket && message) {
       const msgToSend = {
+        type: 'clientMessage',
         sender: userName,
         content: message
-      }
-      socket.send(JSON.stringify(msgToSend))
-      setMessage('')
-      setMessages(prevMessages => [...prevMessages, msgToSend])
+      };
+      socket.send(JSON.stringify(msgToSend));
+      setMessage('');
+      setMessages(prevMessages => [...prevMessages, msgToSend]);
     }
-  }
+  };
+  
 
   useEffect(() => {
     if (isLoggedIn) {
@@ -51,15 +55,18 @@ const Client = () => {
             const newSocket = new WebSocket(`ws://localhost:4000`)
 
             newSocket.addEventListener('message', event => {
-              const serverMessage = JSON.parse(event.data);
+              const serverMessage = JSON.parse(event.data)
               if (serverMessage.type === 'error') {
-                setErrorMessage(serverMessage.content);
-                newSocket.close();
-              } else if (serverMessage.type === 'reply' || !serverMessage.type || serverMessage.sender === 'Manager') {
-                setMessages(prevMessages => [...prevMessages, serverMessage]);
+                setErrorMessage(serverMessage.content)
+                newSocket.close()
+              } else if (
+                serverMessage.type === 'reply' ||
+                !serverMessage.type ||
+                serverMessage.sender === 'Manager'
+              ) {
+                setMessages(prevMessages => [...prevMessages, serverMessage])
               }
-            });
-            
+            })
 
             setSocket(newSocket)
 
@@ -78,46 +85,60 @@ const Client = () => {
     }
   }, [isLoggedIn])
 
+  const toggleMessages = () => {
+    setShowMessages(prevShowMessages => !prevShowMessages)
+  }
+
   return (
     <div className='client'>
-      {!isLoggedIn ? (
-        <form onSubmit={handleLogin}>
-          <label htmlFor='userName'>User Name:</label>
-          <input
-            type='text'
-            id='userName'
-            value={userName}
-            onChange={e => setUserName(e.target.value)}
-            required
-          />
-          <button type='submit'>Login</button>
-        </form>
-      ) : errorMessage ? (
-        <div>{errorMessage}</div>
-      ) : (
-        <>
-          <form onSubmit={handleSendMessage}>
-            <label htmlFor='message'>Message:</label>
-            <input
-              type='text'
-              id='message'
-              value={message}
-              onChange={handleMessageChange}
-            />
-            <button type='submit'>Send</button>
-          </form>
-          <button onClick={handleLogout}>Logout</button>
-          <div className='client-messages'>
-            <h3>Messages</h3>
-            <ul>
-              {messages.map((msg, index) => (
-                <li key={index}>
-                  <strong>{msg.sender}:</strong> {msg.content}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </>
+      <button className='message-icon' onClick={toggleMessages}>
+        &#x1f4ac;
+      </button>
+      {showMessages && (
+        <div className='message-box'>
+          {!isLoggedIn ? (
+            <form onSubmit={handleLogin}>
+              <label htmlFor='userName'>User Name:</label>
+              <input
+                type='text'
+                id='userName'
+                value={userName}
+                onChange={e => setUserName(e.target.value)}
+                required
+              />
+              <button type='submit'>Login</button>
+            </form>
+          ) : errorMessage ? (
+            <div>{errorMessage}</div>
+          ) : (
+            <>
+              <div className='client-messages'>
+                <h3>Messages</h3>
+                <ul>
+                  {messages.map((msg, index) => (
+                    <li
+                      key={index}
+                      className={msg.sender === userName ? 'sent' : 'received'}
+                    >
+                      <strong>{msg.sender}:</strong> {msg.content}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <form onSubmit={handleSendMessage}>
+                <label htmlFor='message'>Message:</label>
+                <input
+                  type='text'
+                  id='message'
+                  value={message}
+                  onChange={handleMessageChange}
+                />
+                <button type='submit'>Send</button>
+              </form>
+              <button onClick={handleLogout}>Logout</button>
+            </>
+          )}
+        </div>
       )}
     </div>
   )
